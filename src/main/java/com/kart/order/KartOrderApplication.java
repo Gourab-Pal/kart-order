3c630b01b46287e65ab2bd39c744efbee8646d28
Gourab-Pal <gourab.pal.gpal@gmail.com>
2026-09-18 23:58:54 +0530

    initial codes

---
+++ src/main/java/com/kart/order/KartOrderApplication.java
@@ -0,0 +1,13 @@
+package com.kart.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KartOrderApplication.class, args);
+    }
+
+}
