e7732a534c30ad3006a2a75b2af461bd1b0d57ab
Gourab-Pal <gourab.pal.gpal@gmail.com>
2026-09-19 00:40:55 +0530

    fix

--- src/main/java/com/kart/order/inventory/repository/InventoryRepository.java
+++ src/main/java/com/kart/order/inventory/repository/InventoryRepository.java
@@ -1,7 +1,7 @@
 package com.kart.order.inventory.repository;
 
 import com.kart.order.inventory.entity.InventoryEntity;
-import org.hibernate.internal.util.Optional;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
